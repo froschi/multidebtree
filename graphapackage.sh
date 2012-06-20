7c690db60ce18959a8a05e173f58edd81ac1d7b8
Thorsten Fischer <thorsten@froschi.org>
2012-06-20 12:48:25 +0200

    graphapackage.sh: Handle the case of single output files.
    Sometimes, only one PDF file would be created, because a package
    is only present in one distribution. In such a case, the call to
    pdfunite would fail, because it expects at least two sourece
    files to unite.
    
    We now collect the names of successfully created PDFs in an array.
    At the end, we check the number of items in the array. If there is
    only one item in there, we rename that file to be the final output
    PDF. For the case of more than one file, the wildcard call to
    pdfunite is retained.
    
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

---
+++ graphapackage.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -x
+
+MULTIDEB_ROOT=$HOME/.multideb
+ARCH=amd64
+DISTS="\
+  lucid\
+  maverick\
+  natty\
+  oneiric\
+  precise\
+  quantal\
+"
+
+PKG_NAME=$1
+
+# pdfunite won't work with just one input file; however, it happens
+# often that only one gets created. So we add all successfully created
+# PDF names to an array, count the mebers in the end, and if there is
+# only one member in the array, we simply mv that file to become the
+# end product.
+pdfs=( )
+
+for dist in $DISTS; do
+  echo "[+] Processing $dist ..."
+  APT_ROOT="$MULTIDEB_ROOT/$dist/$ARCH"
+
+  DOT_FILE=$dist-$ARCH-$PKG_NAME.dot
+  PDF_FILE=$dist-$ARCH-$PKG_NAME.pdf
+  APT_CONFIG=$APT_ROOT/apt.conf debtree \
+    --no-conflicts \
+    --no-alternatives \
+    --no-recommends \
+    --no-provides \
+    --condense \
+    $PKG_NAME \
+  2>/dev/null > $DOT_FILE
+  if [ "$?" -eq "0" ]; then
+    pdfs=( "${pdfs[@]}" "$PDF_FILE" )
+    sed "2i label=\"$PKG_NAME ($dist)\";" -i $DOT_FILE
+    sed "3i labelloc=t;" -i $DOT_FILE
+    dot -Tpdf -o $PDF_FILE $DOT_FILE
+  fi
+done
+
+if [ "${#pdfs[*]}" -gt "1" ]; then
+  pdfunite *-$PKG_NAME.pdf $PKG_NAME.pdf
+else
+  mv ${pdfs[0]} $PKG_NAME.pdf
+fi
+make clean
