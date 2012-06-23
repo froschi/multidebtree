b316dede583d0b1f7aa8c3779acce02636e52d3d
Thorsten Fischer <thorsten@froschi.org>
2012-06-24 01:04:38 +0200

    dep_print.py: Next step: added a processing queue.
    Which gets filled with the dependencies, and processes those.
    
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- dep_print.py
+++ dep_print.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
+from collections import deque
 import apt
+import os
 import pprint
 import sys
 
@@ -10,23 +12,44 @@ pp = pprint.PrettyPrinter()
 endlist = []   # endlist from debtree config
 skiplist = []  # skiplist from debtree config
 deplist = []   # list of dependencies of a particular package.
+extlist = []  # list of existing cookbooks
+donelist = [] # list of things already processed this session.
+workdeque = deque()
 
 endfile = "/home/thorsten/.debtree/endlist"
 skipfile = "/home/thorsten/.debtree/skiplist"
+endlist = [line.strip() for line in open(endfile)]
+skiplist = [line.strip() for line in open(skipfile)]
+
+cookbook_path = "/home/thorsten/scm/code/chef/cookbooks/"
+extlist = [dname for dname in os.listdir(cookbook_path)]
 
 def list_diff(list1, list2):
   return list(set(list1) - set(list2))
 
-endlist = [line.strip() for line in open(endfile)]
-skiplist = [line.strip() for line in open(skipfile)]
-
-pkg_name = sys.argv[1]
+def process_package(pkg_name):
+  pkg = cache[pkg_name]
+  if pkg in donelist:
+    return
+  donelist.append(pkg) 
+  deps = pkg.versions[0].dependencies
+  deplist = [dep[0].name for dep in deps]
+  deplist = list_diff(deplist, skiplist)
+  more_list = list_diff(deplist, endlist)
+  more_list = list_diff(more_list, donelist)
+  more_list = list_diff(more_list, extlist)
+  print ("%s:%s") % (pkg_name, ','.join(deplist))
+  for m in more_list:
+    workdeque.append(m)
 
 cache = apt.Cache()
-pkg = cache[pkg_name]
+pkg_name = sys.argv[1]
 
-deps = pkg.versions[0].dependencies
-deplist = [dep[0].name for dep in deps]
-deplist = list_diff(deplist, skiplist)
+workdeque.append(pkg_name)
 
-print ("%s:%s") % (pkg_name, ','.join(deplist))
+while True:
+  try:
+    process_package(workdeque.pop())
+  except:
+    break
+
