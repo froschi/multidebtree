626afcf897c64a646b624122a4c5db2327b16412
Thorsten Fischer <thorsten@froschi.org>
2012-06-25 17:44:58 +0200

    dep_print.py: Play with colouring in the debug output.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- dep_print.py
+++ dep_print.py
@@ -16,6 +16,8 @@ extlist = []  # list of existing cookbooks
 donelist = [] # list of things already processed this session.
 workdeque = deque()
 
+package_mappings = ['libgcrypt11']
+
 endfile = "/home/thorsten/.debtree/endlist"
 skipfile = "/home/thorsten/.debtree/skiplist"
 endlist = [line.strip() for line in open(endfile)]
@@ -35,10 +37,19 @@ def process_package(pkg_name):
   deps = pkg.versions[0].dependencies
   deplist = [dep[0].name for dep in deps]
   deplist = list_diff(deplist, skiplist)
+  if pkg_name in package_mappings:
+      print "\033[92m%s\033[0m(+):" % pkg_name,
+  else:
+      print "\033[91m%s\033[0m:" % pkg_name,
+  for d in deplist:
+    if d in package_mappings:
+      print "\033[92m%s\033[0m(-)" % d,
+    else:
+      print "\033[91m%s\033[0m" % d,
+  print
   more_list = list_diff(deplist, endlist)
   more_list = list_diff(more_list, donelist)
   more_list = list_diff(more_list, extlist)
-  print ("%s:%s") % (pkg_name, ','.join(deplist))
   for m in more_list:
     workdeque.append(m)
 
@@ -52,4 +63,3 @@ while True:
     process_package(workdeque.pop())
   except:
     break
-
